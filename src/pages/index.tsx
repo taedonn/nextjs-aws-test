ef2530f60c5916beb662b3082103e59e7ff6e5e0
taeho <taedonn@taedonn.com>
2023-09-20 17:08:38 +0900

    update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -78,8 +78,17 @@ const Index = ({params}: any) => {
                 <div className="w-[580px] p-[16px] border rounded-[12px] border-theme-7">
                     {
                         imgDisplay
-                        // eslint-disable-next-line @next/next/no-img-element
-                        ? <img src={imgUrl} alt="" className="w-[100%] rounded-[12px]"/>
+                        ? <div className="relative rounded-[12px] overflow-hidden">
+                            {/* eslint-disable-next-line @next/next/no-img-element */}
+                            <img src={imgUrl} alt="" className="w-[100%]"/>
+                            {
+                                isLoading
+                                ? <div className="w-[100%] h-[100%] absolute z-10 left-0 top-0 flex justify-center items-center bg-theme-10/60">
+                                    <span className="loader"></span>
+                                </div>
+                                : <></>
+                            }
+                        </div>
                         : <div className="w-[100%] h-[300px] relative rounded-[12px] overflow-hidden flex flex-col justify-center items-center bg-theme-9/60">
                             <svg className="w-[180px] fill-theme-8/60" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
                                 <path d="M.002 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2V3zm1 9v1a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V9.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12zm5-6.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0z"/>
